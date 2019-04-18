9f591eb8bd866b278b487f11809af444afa5bf76
Private Chen <chenbbml@gmail.com>
2019-04-19 00:29:31 +0800

    commit_20190419

--- zxing-master/app/src/main/java/com/yzq/zxing/ProductActivity.java
+++ zxing-master/app/src/main/java/com/yzq/zxing/ProductActivity.java
@@ -4,20 +4,40 @@ import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.content.Intent;
-import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.PopupWindow;
 
-public class ProductActivity extends AppCompatActivity {
+import java.io.File;
+import java.io.FileOutputStream;
+
+import service.ImgService;
+
+public class ProductActivity extends AppCompatActivity implements EditProductFragment.OnEditProductFragmentListener {
 
     public static final int RESULT_LOAD_IMAGE = 1002;
     public static final int RESULT_CAMERA_IMAGE = 1003;
 
+    private Fragment currentFragment;
+
     private String code;
     private String action;
+
+    private Uri imageUri;
+
+    private ImgService imgService = new ImgService();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,12 +53,12 @@ public class ProductActivity extends AppCompatActivity {
         action = intent.getStringExtra("action");
         switch (action){
             case "search":
-                ShowProductFragment showProductFragment = ShowProductFragment.newInstance(code);
-                replaceFragment(showProductFragment);
+                currentFragment = ShowProductFragment.newInstance(code);
+                replaceFragment(currentFragment);
                 break;
             case "edit":
-                EditProductFragment editProductFragment = EditProductFragment.newInstance(code);
-                replaceFragment(editProductFragment);
+                currentFragment = EditProductFragment.newInstance(code);
+                replaceFragment(currentFragment);
                 break;
                 default:
         }
@@ -60,9 +80,110 @@ public class ProductActivity extends AppCompatActivity {
             if (requestCode == RESULT_LOAD_IMAGE && null != data) {
 
             }else if (requestCode == RESULT_CAMERA_IMAGE) {
+                try{
+                    Bitmap bitmap = BitmapFactory.decodeStream(
+                            getContentResolver().openInputStream(imageUri));
+                    File imageFile = saveMyBitmap(bitmap);
+                    imgService.uploadImage(imageFile);
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
 
             }
         }
+    }
+
+    public File saveMyBitmap(Bitmap mBitmap){
+        File storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM);
+        File file = null;
+        try {
+            file = File.createTempFile(
+                    "temp_image",  /* prefix */
+                    ".jpg",         /* suffix */
+                    storageDir      /* directory */
+            );
+
+            FileOutputStream out=new FileOutputStream(file);
+            mBitmap.compress(Bitmap.CompressFormat.JPEG, 20, out);
+            out.flush();
+            out.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return  file;
+    }
+
+    public void showPopueWindow(){
+        View popView = View.inflate(this,R.layout.popup_camera_need_view,null);
+        Button bt_album = popView.findViewById(R.id.btn_pop_album);
+        Button bt_camera = popView.findViewById(R.id.btn_pop_camera);
+        Button bt_cancel = popView.findViewById(R.id.btn_pop_cancel);
+        //获取屏幕宽高
+        int weight = getResources().getDisplayMetrics().widthPixels;
+        int height = getResources().getDisplayMetrics().heightPixels*1/3;
+
+        final PopupWindow popupWindow = new PopupWindow(popView,weight,height);
+        //popupWindow.setAnimationStyle(R.style.anim_popup_dir);
+        popupWindow.setFocusable(true);
+        //点击外部popueWindow消失
+        popupWindow.setOutsideTouchable(true);
+
+        bt_album.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                popupWindow.dismiss();
+
+            }
+        });
+        bt_camera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                File outputImage = new File(getExternalCacheDir(),
+                        "output_image.jpg");
+                try{
+                    if(outputImage.exists()){
+                        outputImage.delete();
+                    }
+                    outputImage.createNewFile();
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+                if(Build.VERSION.SDK_INT >= 24){
+                    imageUri = FileProvider.getUriForFile(ProductActivity.this,
+                            "com.yzq.zxing.fileprovider",outputImage);
+                }else{
+                    imageUri = Uri.fromFile(outputImage);
+                }
+                Intent intent = new Intent("android.media.action.IMAGE");
+                intent.putExtra(MediaStore.EXTRA_OUTPUT,imageUri);
+                startActivityForResult(intent,RESULT_CAMERA_IMAGE);
+                popupWindow.dismiss();
+
+            }
+        });
+        bt_cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                popupWindow.dismiss();
+
+            }
+        });
+        //popupWindow消失屏幕变为不透明
+        popupWindow.setOnDismissListener(new PopupWindow.OnDismissListener() {
+            @Override
+            public void onDismiss() {
+                WindowManager.LayoutParams lp = getWindow().getAttributes();
+                lp.alpha = 1.0f;
+                getWindow().setAttributes(lp);
+            }
+        });
+        //popupWindow出现屏幕变为半透明
+        WindowManager.LayoutParams lp = getWindow().getAttributes();
+        lp.alpha = 0.5f;
+        getWindow().setAttributes(lp);
+        popupWindow.showAtLocation(popView, Gravity.BOTTOM,0,50);
+
     }
 
 }
